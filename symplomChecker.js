4e3d5019b96cfbb03c1626d787c26ee48f688a47
Varad Gupta <varadgupta21@gmail.com>
2024-01-10 12:26:47 +0530

    some changes

--- symplomChecker.js
+++ symplomChecker.js
@@ -27,15 +27,9 @@ async function run(data) {
 13. Alcohol Intake: ${data.alcohol_intake}
 14. Occasional Drug Use: ${data.occasional_drug_use}
   `;
-  console.log(1)
-  console.log(prompt)
   const result = await model.generateContent(prompt);
-  console.log(2)
   const response = await result.response;
-  console.log(3)
   const text = response.text();
-  console.log(4)
-  console.log(text)
   return text;
 }
 
