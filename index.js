5767a2778085dd8fd97011bd444ca8d7fef33705
Varad Gupta <varadgupta21@gmail.com>
2024-01-10 11:28:03 +0530

    added body-parser

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const run = require('./symplomChecker')
 const dotenv = require('dotenv')
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 dotenv.config()
 const port = process.env.port
 app.use(express.json())
